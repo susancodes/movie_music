58604363e4a9af2d83b7567e063b68b305e80715
Susan Chin <susancodes@gmail.com>
2017-10-21 17:39:10 -0700

    modularize functions

--- movie_music.py
+++ movie_music.py
@@ -3,28 +3,16 @@ import json
 import requests
 import urllib
 
+
 SPOTIFY_ACCESS_TOKEN = os.environ.get('SPOTIFY_ACCESS_TOKEN')
 
-genre_reponse = requests.get('https://api.themoviedb.org/3/genre/movie/list?api_key=eb90ad0ff308c0b16691816876d0f9f4')
-genre_text = json.loads(genre_reponse.text).get('genres')
 
-genre_ids = [item.get('id') for item in genre_text]
+def get_all_movie_titles_for_genre(genre_id):
+	movie_res = requests.get('https://api.themoviedb.org/3/genre/{}/movies?api_key=eb90ad0ff308c0b16691816876d0f9f4'.format(genre_id))
+	movie_list = json.loads(movie_res.text).get('results')
+	for movie in movie_list:
+		yield movie.get('title')
 
-
-def get_all_movie_titles(genre_ids):
-	all_movie_titles = []
-
-	# grabbing all movies' titles
-	for genre_id in genre_ids:
-		movie_res = requests.get('https://api.themoviedb.org/3/genre/{}/movies?api_key=eb90ad0ff308c0b16691816876d0f9f4'.format(genre_id))
-		movie_list = json.loads(movie_res.text).get('results')
-		all_movie_titles.extend(item.get('title') for item in movie_list)
-
-	return all_movie_titles
-
-
-# grab all of the soundtrack popularity for each movie
-for movie in all_movie_titles:
 
 def get_movie_soundtrack_popularity(movie):
 
@@ -38,17 +26,34 @@ def get_movie_soundtrack_popularity(movie):
 	st_res = requests.get('https://api.spotify.com/v1/search', headers=headers, params=params)
 
 	album_text = json.loads(st_res.text).get('albums')
-
 	if not album_text:
 		continue
 
 	album_items = album_text.get('items')
-
 	if not album_items:
 		continue
 
 	# we only asked for the first result in our api request so we know it's the first item and we've already checked that the list exists
 	album_id = album_items[0].get('id')
-
 	album_res = requests.get('https://api.spotify.com/v1/albums/{}'.format(album_id), headers=headers)
 	return json.loads(album_res.text).get('popularity')
+
+
+def get_movie_soundtrack_popularity_avg_by_genre(genre_id):
+	movie_titles = get_all_movie_titles_for_genre(genre_id)
+	total_score = 0
+
+	for movie_count, movie in enumerate(movie_titles):
+		total_score += get_movie_soundtrack_popularity(movie)
+
+	return (total_score / movie_count)
+
+def get_movie_music_popularity_scores():
+	genre_reponse = requests.get('https://api.themoviedb.org/3/genre/movie/list?api_key=eb90ad0ff308c0b16691816876d0f9f4')
+	all_genre_list = json.loads(genre_reponse.text).get('genres')
+
+	for genre_dict in all_genre_list:
+		return {
+			'name': genre_dict.get('name'),
+			'popularity_avg': get_movie_soundtrack_popularity_avg_by_genre(genre_dict.get('id'))
+		}
